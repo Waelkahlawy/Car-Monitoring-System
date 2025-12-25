b37b6504ef523a84632cb0c5e3b66ab6d81d6838
Wael Kahlawy <waelkahlawi@gmail.com>
2025-12-25 23:50:38 +0200

    update Cfg file

--- Src/Cfg.h
+++ Src/Cfg.h
@@ -1,7 +1,29 @@
 #ifndef CFG_H
 #define CFG_H
 
-// Configuration file for the Car Monitoring System
+#include <stdint.h>
+
+// Standard definitions
+#define STD_ON      1 // Enabled
+#define STD_OFF     0   // Disabled
+
+#define ADC_ENABLED     STD_ON
+#define TAG_GPIO        "GPIO_DRIVER"
+#define TAG_ADC         "ADC_DRIVER"
 
 
-#endif // CFG_H
+// Enable or disable drivers
+#define GPIO_ENABLED            STD_ON
+#define ADC_ENABLED             STD_ON
+
+// Enable or disable debug logs for drivers
+// GPIO Debugging
+#if GPIO_ENABLED == STD_ON
+#define GPIO_DEBUG_ENABLED      STD_ON
+#endif
+// ADC Debugging
+#if ADC_ENABLED == STD_ON
+#define ADC_DEBUG_ENABLED       STD_ON
+#endif 
+
+#endif 
