986f6908ae8cf46565fd3858362f801e2e4a5fc8
Wael Kahlawy <waelkahlawi@gmail.com>
2025-12-26 21:05:02 +0200

    Update main file

--- main/main.c
+++ main/main.c
@@ -1,3 +1,10 @@
+#include "../Src/Hal/WIFI/wifi.h"
+#include "../Src/Cfg.h"
+#include "../Src/Hal/GPIO/gpio.h"
+#include "../Src/Hal/ADC/adc.h"
+#include "../Src/Hal/I2C/i2c.h"
+#include "../Src/Hal/UART/uart.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
@@ -6,3 +13,4 @@ void app_main(void)
 {
    
 }
+
