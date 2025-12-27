c8b1e9be3ab45b08ba6f8b39ef65e6c88a08cd81
Wael Kahlawy <waelkahlawi@gmail.com>
2025-12-28 01:27:57 +0200

    Update

--- main/main.c
+++ main/main.c
@@ -4,13 +4,26 @@
 #include "../Src/Hal/ADC/adc.h"
 #include "../Src/Hal/I2C/i2c.h"
 #include "../Src/Hal/UART/uart.h"
+#include "../Src/App/IMU/imu.h"
+#include "../Src/Hal/WIFI/wifi.h"
+#include "../Src/Hal/MQTT/mqtt.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <unistd.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+
+
 
 void app_main(void)
 {
-   
+    
 }
 
+ 
+    
